359e70b06a6f77fa12c2ea90964e15faf9d1429f
Kiran <kirankatariya8778@gmail.com>
2016-04-04 20:18:54 +0530

    Just for the image(jpg/jpeg/png) files

--- app/routes/superhero.js
+++ app/routes/superhero.js
@@ -1,30 +1,43 @@
-var mongoose = require('mongoose');
-var Superhero = require('../models/superhero');
-
-module.exports = function(){
+//Dependencies
+var mongoose        = require('mongoose');
+var Superhero       = require('../models/superhero');
+//App routes
+module.exports = function() {
 	return {
-		getAll: function(req, res){
+		/*
+		 * Get route to retrieve all the superheroes.
+		 */
+		getAll : function(req, res){
+			//Query the DB and if no errors, send all the superheroes
 			var query = Superhero.find({});
 			query.exec(function(err, superheroes){
 				if(err) res.send(err);
+				//If no errors, send them back to the client
 				res.json(superheroes);
 			});
 		},
-
+		/*
+		 * Post route to save a new superhero into the DB.
+		 */
 		post: function(req, res){
-			var newSuperhero =  new Superhero(req.body);
+			//Creates a new superhero
+			var newSuperhero = new Superhero(req.body);
+			//Save it into the DB.
 			newSuperhero.save(function(err){
 				if(err) res.send(err);
-				res.json(req.body)
+				//If no errors, send it back to the client
+				res.json(req.body);
 			});
-
 		},
-
+		/*
+		 * Get a single superhero based on id.
+		 */
 		getOne: function(req, res){
-            Superhero.findById(req.params.id, function(err, superhero){
-                if(err) res.send(err);	
-                res.json(superhero);
-            });     
-        }
+			Superhero.findById(req.params.id, function(err, superhero){
+				if(err) res.send(err);
+				//If no errors, send it back to the client
+				res.json(superhero);
+			});		
+		}
 	}
-};
+};  
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var port = process.env.PORT || 3000;
+var port = 3000;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
@@ -8,33 +8,28 @@ var path = require('path');
 
 var superhero = require('./app/routes/superhero')();
 
-var options = {
-	server: {
-		socketOptions: {keepAlive: 1,  connectTimeoutMS: 30000}
-	},
-	replset: {
-		socketOptions: {keepAlive: 1,  connectTimeoutMS: 30000}
-	}
-};
-
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
 mongoose.connect('mongodb://localhost/File_Upload', options);
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error'));
+db.on('error', console.error.bind(console, 'connection error:'));
 
+//Log with Morgan
 app.use(morgan('dev'));
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));  
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/json'}));
-
-app.use(express.static(__dirname + '/public'));
+//Static files
+app.use(express.static(__dirname + '/public')); 
 
 app.route('/superhero')
 	.post(superhero.post)
 	.get(superhero.getAll);
-app.route('superhero?:id')
+app.route('/superhero/:id')
 	.get(superhero.getOne);
 
 app.listen(port);
